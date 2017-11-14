e7a1ee5abc880edd6668d78958a973ccaee3ed0b
Jaime Pericas Saez <jaimefps@Jaimes-MacBook-Pro.local>
2017-11-13 17:44:54 -0800

    fix bug that removes images from anchors

--- content.js
+++ content.js
@@ -28,12 +28,13 @@ function R_ComoEnPR(str) {
 function S_ComoEnPR(str) {
   return str.replace(/s(?=[^aeiou]|$)/g, 'j');
 }
-const types = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
+const types = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a'];
 
 for (type of types) {
   const elements = document.getElementsByTagName(type);
+  console.log('test', elements)
   for ( el of elements ) {
-    if (typeof el.innerHTML == 'string') {
+    if (!el.innerHTML.match(/\<img/)) {
       el.innerHTML = S_ComoEnPR( R_ComoEnPR(el.innerHTML) );
     }
   }
