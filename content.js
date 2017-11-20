596f3043bcb6be52deac3b963406ffdc6e4f0667
Jaime Pericas Saez <jaimefps@Jaimes-MacBook-Pro.local>
2017-11-20 06:07:42 -0400

    .

--- content.js
+++ content.js
@@ -1,24 +1,34 @@
-/**
- * how puertorricans substitute 'r' for 'l' in some cases:
- */
-function R_ComoEnPR(str) {
-  return str.replace(/r(?=[^aeiouráéíóú]|$)/g, 'l');   
+const Borikense = {
+  R_ComoEnPR(str) {
+    return str.replace(/r(?![raeiouáéíóú])/g, 'l');   
+  },
+  S_ComoEnPR(str) {
+    return str.replace(/s(?![aeiouáéíóú])/g, 'j');
+  },
+  Ado_ComoEnPR (str) {
+    return str.replace(/ado(?=[\s\.])/g, 'au')
+  },
 }
 
-/**
- * how puertorricans substitute 's' for an aspirated sound:
- */
-function S_ComoEnPR(str) {
-  return str.replace(/s(?=[^aeiouáéíóú]|$)/g, 'j');
+const htmlTypes = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a'];
+
+function applyAccent (str, accents) {
+  let mem = str
+  for (let regex in accents) {
+    mem = accents[regex](mem)
+  }
+  return mem
 }
 
-const types = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a'];
-
-for (type of types) {
-  const elements = document.getElementsByTagName(type);
-  for (el of elements) {
-    if (!el.innerHTML.match(/\<img/)) {
-      el.innerHTML = S_ComoEnPR(R_ComoEnPR(el.innerHTML));
+function filterHTML () {
+  for (type of htmlTypes) {
+    const elements = document.getElementsByTagName(type);
+    for (el of elements) {
+      if (!el.innerHTML.match(/\<img/)) {
+        el.innerHTML = applyAccent(el.innerHTML, Borikense)
+      }
     }
   }
 }
+
+filterHTML();
